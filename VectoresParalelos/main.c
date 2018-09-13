f7554c79ce2498fcf716ae5b4856eacf96db49f3
sanrouaux <sanrouaux@gmail.com>
2018-09-13 19:50:57 -0300

    Funcion ordenAlfabetico terminada

--- VectoresParalelos/main.c
+++ VectoresParalelos/main.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#define T 5
+#include "biblio.h"
+#define T 4
 
-void cargarAlumnos(int[], char[][21], int[], float[], int);
-void mostrarAlumnos(int[], char[][21], int[], float[], int);
-void ordenarNombres(char[][21], int);
 
 int main()
 {
@@ -35,7 +33,7 @@ int main()
         {
 
         case 1:
-            ordenarNombres(nombres, T);
+            ordenAlfabetico(legajos, nombres, notas, alturas, T);
             break;
 
         case 2:
@@ -58,26 +56,68 @@ int main()
             break;
 
         default:
-            printf("Ingrese una opcion valida");
+            printf("Ingrese una opcion valida\n");
             system("pause");
         }
-
     }
     while(opcion != 6);
     return 0;
 }
 
 
-void ordenarNombres(char nombres[][21], int tamano)
+void cargarAlumnos(int legajos[], char nombres[][21], int notas[], float alturas[], int tamano)
+{
+    int i;
+    for(i = 0; i < tamano; i++)
+    {
+        printf("Ingrese legajo: ");
+        scanf("%d", &legajos[i]);
+
+
+        printf("Ingrese nombre: ");
+        fflush(stdin);
+        char buffer[1024];
+        gets(buffer);
+        while(strlen(buffer)>20)
+        {
+            printf("Error. Reingrese nombre: ");
+            fflush(stdin);
+            gets(buffer);
+        }
+        strcpy(nombres[i], buffer);
+
+
+        printf("Ingrese nota: ");
+        scanf("%d", &notas[i]);
+
+
+        printf("Ingrese altura: ");
+        scanf("%f", &alturas[i]);
+    }
+}
+
+
+void mostrarAlumnos(int legajos[], char nombres[][21], int notas[], float alturas[], int tamano)
+{
+    printf("%6s %20s %4s %5s\n","Legajo","Nombre","Nota","Altura");
+
+    int i;
+    for(i=0; i < tamano; i++)
+    {
+        printf("%4d %20s %2d %5f \n", legajos[i], nombres[i], notas[i], alturas[i]);
+    }
+}
+
+
+void ordenAlfabetico(int legajos[], char nombres[][21], int notas[], float alturas[], int tamano)
 {
     int i;
     int j;
     char aux[21];
+    int legajoAuxiliar;
+    int notaAuxiliar;
+    int alturaAuxiliar;
 
-    /*for(i = 0; i < tamano; i++)
-    {
-        strlwr(nombres[i]);
-    }*/
 
     for(i = 0; i < tamano -1; i++)
     {
@@ -85,51 +125,30 @@ void ordenarNombres(char nombres[][21], int tamano)
         {
             if(tolower(nombres[j][0]) < tolower(nombres[i][0]))
             {
-                    strcpy(aux, nombres[j]);
-                    strcpy(nombres[j], nombres[i]);
-                    strcpy(nombres[i], aux);
+                legajoAuxiliar = legajos[j];
+                legajos[j] = legajos[i];
+                legajos[i]= legajoAuxiliar;
+
+                strcpy(aux, nombres[j]);
+                strcpy(nombres[j], nombres[i]);
+                strcpy(nombres[i], aux);
+
+                notaAuxiliar = notas[j];
+                notas[j] = notas[i];
+                notas[i]= notaAuxiliar;
+
+                alturaAuxiliar = alturas[j];
+                alturas[j] = alturas[i];
+                alturas[i]= alturaAuxiliar;
             }
         }
     }
 
-
-    for(i = 0; i < tamano; i++)
-    {
-        puts(nombres[i]);
-    }
-}
-
-
-void cargarAlumnos(int legajos[], char nombres[][21], int notas[], float alturas[], int dimension)
-{
-    int i;
-    for(i = 0; i < dimension; i++)
-    {
-        printf("Ingrese legajo: ");
-        scanf("%d", &legajos[i]);
-
-        printf("Ingrese nombre: ");
-        fflush(stdin);
-        gets(nombres[i]);
-
-        printf("Ingrese nota: ");
-        scanf("%d", &notas[i]);
-
-        printf("Ingrese altura: ");
-        scanf("%f", &alturas[i]);
-    }
-}
-
-void mostrarAlumnos(int legajos[], char nombres[][21], int notas[], float alturas[], int dimension)
-{
-    printf("%4s %20s %2s %5s\n","Legajo","Nombre","Nota","Altura");
-
-    int i;
-    for(i=0; i<dimension; i++)
-    {
+    printf("%6s %20s %4s %5s\n","Legajo","Nombre","Nota","Altura");
+    for(i=0; i < tamano; i++)
         printf("%4d %20s %2d %5f \n", legajos[i], nombres[i], notas[i], alturas[i]);
-    }
 }
+
 
 
 /*
